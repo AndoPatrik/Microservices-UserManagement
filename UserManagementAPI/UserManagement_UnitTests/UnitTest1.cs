9a9495378f15dd24487c92ebeefe47ee6160124c
dani337x <dani337x@edu.easj.dk>
2020-04-24 11:01:16 +0200

    Unit testing project has been added

---
+++ UserManagementAPI/UserManagement_UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserManagementAPI;
+
+namespace UserManagement_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
